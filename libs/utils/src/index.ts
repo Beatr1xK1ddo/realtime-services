465de53f22d362382a36fc64a39062dde2c7db4a
bender <sergeyh@nextologies.com>
2022-03-25 16:17:39 +0300

    libs/utils/src  - utils initial impl package.json package-lock.json  - dependencies update

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,92 +1,20 @@
-import { readFileSync, existsSync, writeFileSync, appendFile } from 'fs';
-import * as _ from 'lodash';
 import * as shell from 'child_process';
-import * as ipRegex from 'ip-port-regex';
-import { v4 as uuidv4 } from 'uuid';
-import * as loadIniFile from 'read-ini-file';
-import * as url from 'url';
-import * as conf from '../config.json';
 
-export default {
-    isValidIP(ip) {
-        return ipRegex({ exact: true }).test(ip.split(':').shift());
-    },
-    getHostNodeID() {
-        return loadIniFile.sync(conf.hostConfigIni).instance_id;
-    },
-    generateToken() {
-        return uuidv4();
-    },
-    getReqQuery(req) {
-        const { query } = url.parse(req.url, true);
-        return query;
-    },
-    resolvePath(filepath) {
-        return __dirname + '/' + filepath;
-    },
-    currentTime() {
-        return Math.round(new Date().getTime() / 1000);
-    },
-    exec(cmd: string) {
-        return new Promise((resolve, reject) => {
-            shell.exec(cmd, (err: any, output: any) => {
-                err ? reject(err) : resolve(output);
-            });
+function exec(cmd: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+        shell.exec(cmd, (err: any, output: any) => {
+            err ? reject(err) : resolve(output);
         });
-    },
-    async getCache(key: string, saveCallback: any) {
-        let dataContent: any = null;
+    });
+}
 
-        try {
-            const cacheFile = `${conf.logDir}${key}`;
+async function getNodeId(): Promise<number | null> {
+    try {
+        const id = await exec("/usr/bin/php /root/dv_control_new.php hostname");
+        return Number.parseInt(id);
+    } catch (e) {
+        return null;
+    }
+}
 
-            let isExpired = false;
-
-            if (existsSync(cacheFile)) {
-                const { expires, data } = JSON.parse(
-                    readFileSync(cacheFile, 'utf8')
-                );
-
-                if (+expires < this.currentTime()) {
-                    isExpired = true;
-                } else {
-                    dataContent = data;
-                }
-            } else {
-                isExpired = true;
-            }
-
-            if (isExpired) {
-                dataContent = await saveCallback();
-
-                setTimeout(() => {
-                    writeFileSync(
-                        cacheFile,
-                        JSON.stringify({
-                            expires: this.currentTime() + 1800,
-                            data: dataContent || null,
-                        })
-                    );
-                });
-            }
-        } catch (e) {
-            dataContent = null;
-        }
-
-        return Promise.resolve(dataContent || null);
-    },
-    log(message: string) {
-        try {
-            appendFile(
-                conf.logDir + 'common_log.txt',
-                new Date().toUTCString() + '  ' + message + '\n',
-                (err) => {
-                    err && console.log(err.toString());
-                }
-            );
-        } catch (e) {}
-    },
-    getPort(url: string) {
-        return _.trim(url.split(':').slice(-1).pop(), '/');
-    },
-};
+export {exec, getNodeId};
