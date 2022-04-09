c444eb4350947d63649709d466057a8283ec249d
bender <sergeyh@nextologies.com>
2022-04-09 15:51:33 +0300

    libs/node-services/teranex/src/lib/teranexNodeService.ts  - handleSubscription event handling

--- libs/node-services/teranex/src/lib/teranexNodeService.ts
+++ libs/node-services/teranex/src/lib/teranexNodeService.ts
@@ -1,5 +1,5 @@
 import {
-    IClientCmdRequestEvent,
+    IClientCmdRequestEvent, IClientSubscribeEvent,
     IDeviceResponseEvent,
     NodeService,
 } from '@socket/shared-types';
@@ -11,11 +11,21 @@ export class TeranexNodeService extends NodeService<TeranexDevice> {
             this.logger.log.info('TeranexNodeService connected');
             this.socket.emit('init', { nodeId: this.nodeId });
         });
-
+        this.socket.on('subscribe', this.handleSubscription.bind(this));
         this.socket.on('request', this.handleRequest.bind(this));
-
         this.socket.on('error', (error) => this.logger.log.error(error));
     }
+
+    private async handleSubscription(event: {socketId: string, event: IClientSubscribeEvent}) {
+        const { ip, port } = event.event;
+        const deviceId = `${ip}:${port}`;
+        try {
+            const device = await this.getDevice(deviceId);
+            if (device) this.socket.emit('subscribed', event);
+        } catch (e) {
+            //todo: add subscription failure handling
+        }
+    };
 
     private async handleRequest(data: IClientCmdRequestEvent) {
         const { ip, port, commands } = data;
@@ -23,7 +33,6 @@ export class TeranexNodeService extends NodeService<TeranexDevice> {
 
         try {
             const device = await this.getDevice(deviceId);
-
             const cmdAnswers = await Promise.all(
                 commands.map(async (command: string) => {
                     try {
@@ -65,14 +74,11 @@ export class TeranexNodeService extends NodeService<TeranexDevice> {
         }
 
         const [ip, port] = deviceId.split(':');
-
         const newDevice = await this.createDevice(ip, parseInt(port));
-
         if (!newDevice) {
             this.logger.log.error(`Can not get device ${deviceId}`);
             throw new Error(`Can not get device ${deviceId}`);
         }
-
         this.devices[deviceId] = newDevice;
         return this.devices[deviceId];
     }
