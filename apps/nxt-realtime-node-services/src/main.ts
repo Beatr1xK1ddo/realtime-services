36373c077550999b045572b64b3859030e647e10
bender <sergeyh@nextologies.com>
2022-05-20 10:54:11 +0300

    apps/nxt-realtime-node-services/src  - remove commented service start

--- apps/nxt-realtime-node-services/src/main.ts
+++ apps/nxt-realtime-node-services/src/main.ts
@@ -3,16 +3,14 @@ import {LoggerNodeService} from "@socket/node-services-logger";
 import {TeranexNodeService} from "@socket/node-services-teranex";
 
 import * as config from "./config.json";
-import {HyperdeckNodeService} from "@socket/node-services/hyperdeck";
 
 const {
     logAgent: {applogDir, syslogFile, excludeMessages, serviceUrl: loggerServiceUrl},
     teranex: {serviceUrl: teranexServiceUrl},
 } = config;
 
-// getNodeId().then((id) => {
-//     if (id === null) return;
-//     new LoggerNodeService("Logger", id, loggerServiceUrl, applogDir, syslogFile, excludeMessages);
-//     new TeranexNodeService("Teranex", id, teranexServiceUrl);
-// });
-new HyperdeckNodeService("HyperdeckNode", 1337, "http://localhost:9000/hyperdeck");
+getNodeId().then((id) => {
+    if (id === null) return;
+    new LoggerNodeService("Logger", id, loggerServiceUrl, applogDir, syslogFile, excludeMessages);
+    new TeranexNodeService("Teranex", id, teranexServiceUrl);
+});
