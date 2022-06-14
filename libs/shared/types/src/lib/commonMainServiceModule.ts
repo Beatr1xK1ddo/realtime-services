88e27696b0b08655045d482005a8d948df5c593f
Kan Alexandr <alexander.kan@mirapolis.ru>
2022-06-14 14:13:27 +0300

    qq

---
+++ libs/shared/types/src/lib/commonMainServiceModule.ts
@@ -0,0 +1,16 @@
+import {Namespace} from "socket.io";
+
+import {INodeDeviceServiceBaseEvent} from "./commonNodeService";
+
+export interface IMainServiceModule {
+    name: string;
+    init(io: Namespace): void;
+}
+
+export type IMainServiceModuleDeviceSubscribeEvent = INodeDeviceServiceBaseEvent;
+
+export type IMainServiceModuleDeviceUnsubscribeEvent = INodeDeviceServiceBaseEvent;
+
+export interface IMainServiceModuleDeviceCommandsEvent extends INodeDeviceServiceBaseEvent {
+    commands: string[];
+}
